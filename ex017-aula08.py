022410faae6e57a47009771e66c9915b9fb8fd2f
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-10-30 19:32:54 -0300

    Update ex017-aula08.py

--- ex017-aula08.py
+++ ex017-aula08.py
@@ -2,4 +2,4 @@ from math import hypot
 co = float(input('What is the size of the opposite leg?'))
 ca = float(input('What is the size of the adjacent leg?'))
 hypo = hypot(co, ca)
-print ('The hypotenuse of that rectangle triangle is {}!'.format(int(hypo)))
+print ('The hypotenuse of that rectangle triangle is {:.2f}!'.format(int(hypo)))
