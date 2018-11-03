933464e3b9df459ecddcaa09a57ad333fa9ecc25
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-03 13:25:10 -0300

    Update ex041-aula12(correto).py

--- ex041-aula12(correto).py
+++ ex041-aula12(correto).py
@@ -4,11 +4,11 @@ idade = date.today().year - anodenascimeto
 print(f'O atleta tem {idade} anos')
 if idade <= 9:
     print('Você é um atleta Mirim')
-elif 9 < idade <= 14:
+elif idade <= 14:
     print('Você é um atleta Infantil')
-elif 14 < idade <= 19:
+elif idade <= 19:
     print('Você é um atleta Junior')
-elif 19 < idade <= 25:
+elif idade <= 25:
     print('Você é um atleta Sênior')
 else:
     print('Você é um atleta Master!')
