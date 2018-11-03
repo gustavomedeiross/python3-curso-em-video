ac85bf6abcec1e6db220f9fa39a771e28c83a984
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-03 13:24:43 -0300

    Update ex041-aula12.py

--- ex041-aula12.py
+++ ex041-aula12.py
@@ -1,4 +1,7 @@
-idade = int(input('Atleta, digite a sua idade!'))
+from datetime import date
+anodenascimeto = int(input('Ano de nascimento: '))
+idade = date.today().year - anodenascimeto
+print(f'O atleta tem {idade} anos')
 if idade <= 9:
     print('Você é um atleta Mirim')
 elif 9 < idade <= 14:
@@ -9,3 +12,6 @@ elif 19 < idade <= 25:
     print('Você é um atleta Sênior')
 else:
     print('Você é um atleta Master!')
+
+#pode ser feito dessa maneira, mas a estrutura elif(else if) pressupõe que "caso não seja esse, se for esse, faça isso"
+#portanto, o exercício 41(correto) está com uma resolução melhor.
