0d9c89ede72ccd4ce6f1e928840dd929b5e8ea27
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-03 23:28:34 -0300

    Update ex043-aula12.py

--- ex043-aula12.py
+++ ex043-aula12.py
@@ -4,11 +4,11 @@ altura = float(input('Digite a sua altura'))
 imc = peso/pow(peso, 2)
 if imc < 18.5:
     print('Você está abaixo do peso')
-elif 18.5 <= imc <= 25:
+elif imc < 25:
     print('Você está no peso ideal')
-elif 25 < imc <= 30:
+elif imc <= 30:
     print('Você está no sobrepeso')
-elif 30 < imc <= 40:
+elif imc <= 40:
     print('Você está em estado de obesidade!')
 else:
     print('Você está em estado de obesidade mórbida')
