061002188aa306e887f22918726273b7218d5803
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-03 23:10:30 -0300

    Update ex040-aula12.py

--- ex040-aula12.py
+++ ex040-aula12.py
@@ -3,7 +3,7 @@ n2 = float(input('Digite a sua segunda nota gafanhoto!'))
 m = (n1+n2)/2
 if m < 5:
     print('Você foi \033[31mREPROVADO!\033[m')
-elif 5 <= m < 7:
+elif m < 7:
     print('Você está em \033[33mrecuperação!\033[m')
 elif m >= 7:
     print('Você foi \033[32maprovado!\033[m Muito bem!')
