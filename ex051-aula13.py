db23131801c10a47572f55b5d38982f91d9f59b2
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-04 13:53:55 -0200

    Update ex051-aula13.py

--- ex051-aula13.py
+++ ex051-aula13.py
@@ -2,4 +2,4 @@ n1 = int(input('Digite um primeiro termo de uma P.A. ='))
 r = int(input('Digite a razão de uma P.A. = '))
 ndt = (n1 + r*10)
 for c in range(n1, ndt,  r):
-    print(c)
+    print(c, end='...')
