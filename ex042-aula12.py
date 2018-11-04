a137583bb210232080cd906e8bfcfd1a07e089a6
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-03 23:20:45 -0300

    Update ex042-aula12.py

--- ex042-aula12.py
+++ ex042-aula12.py
@@ -10,7 +10,7 @@ if abs(r2-r3) < r1 < r2 + r3:
         print('Esse é um triângulo equilátero!')
     elif r1 == r2 != r3 or r1 == r3 != r2 or r2==r3 != r1:
         print('Esse triâgulo é isósceles!')
-    elif r1 != r2 != r3:
+    elif r1 != r2 != r3 != r1:
         print('Esse triângulo é escaleno!')
 else:
     print('Esse triângulo não pode ser feito!')
