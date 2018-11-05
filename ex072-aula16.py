981a705e9e926a2c1a9e9e46d71564a9c44f73af
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-05 13:28:38 -0200

    Update ex072-aula16.py

--- ex072-aula16.py
+++ ex072-aula16.py
@@ -1,8 +1,13 @@
 números = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
            'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-
-
-n = int(input('Digite um número!'))
-while n < 0 or n > 20:
-    n = int(input('Erro. Digite novamente'))
-print(f'Você digitou o número {números[n]}')
+while True:
+    n = int(input('Digite um número entre 0 e 20: '))
+    while n < 0 or n > 20:
+        n = int(input('Erro. Digite novamente'))
+    print(f'Você digitou o número {números[n]}')
+    qrcnt = ' '
+    while qrcnt not in 'SsNn':
+        qrcnt = str(input('Você quer continuar? [S/N]')).strip()[0]
+    if qrcnt in 'Nn':
+        break
+print(f'{"FIM":=^30}')
