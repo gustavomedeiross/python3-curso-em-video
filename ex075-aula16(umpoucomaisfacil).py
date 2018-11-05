88b5d109d745b9681a24f8b65a57482937865211
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-05 17:56:11 -0200

    Update ex075-aula16(umpoucomaisfacil).py

--- ex075-aula16(umpoucomaisfacil).py
+++ ex075-aula16(umpoucomaisfacil).py
@@ -1,9 +1,5 @@
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro número: '))
-n3 = int(input('Digite mais um número: '))
-n4 = int(input('Digite o último número: '))
-
-tupla = (n1, n2, n3, n4)
+tupla = (int(input('Digite um número: ')), int(input('Digite outro número: ')),
+         int(input('Digite mais um número: ')), int(input('Digite o último número: ')))
 
 print(f'Você digitou os valores {tupla}')
 print(f'O valor 9 apareceu {tupla.count(9)} vezes')
