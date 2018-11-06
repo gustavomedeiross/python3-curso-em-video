4e87331887c65b3f878abcc516adf7b621c2d2a3
Gustavo Mendes de Medeiros <44384467+mendes13@users.noreply.github.com>
2018-11-06 15:05:43 -0200

    Add files via upload

--- ex095-aula19.py
+++ ex095-aula19.py
@@ -3,6 +3,7 @@ jogador = dict()
 gols = list()
 total = 0
 while True:
+    print('-' * 30)
     jogador['nome'] = str(input('Nome do Jogador: ')).capitalize()
     númdepartidas = int(input(f'Quantas partidas {jogador["nome"]} jogou?'))
     for c in range(0, númdepartidas):
@@ -18,4 +19,20 @@ while True:
         qrcnt = input('Quer continuar? [S/N]')
     if qrcnt in 'Nn':
         break
-print(jogadores)
+print('-=' * 30)
+print(f'{"Código":<9}{"Nome":<10}{"Gols":<20}{"Total":<7}')
+print('-' * 40)
+for c in range(0, len(jogadores)):
+    print(f'{c:<8} {jogadores[c]["nome"]:<10}{str(jogadores[c]["gols"]):<20}{jogadores[c]["total"]:<7}')
+print('-' * 40)
+while True:
+    dados = int(input('Mostrar dados de qual jogador? (999 fecha o programa): '))
+    if dados in range(0, len(jogadores)):
+        print(f'==LEVANTAMENTO DO JOGADOR {jogadores[dados]["nome"]}:')
+        for c in range(0, len(jogadores[dados]['gols'])):
+            print(f'    No jogo {c+1} fez {jogadores[dados]["gols"][c]} gols')
+    elif dados == 999:
+        break
+    else:
+        print('\033[31mJogador Inválido. Tente novamente.\033[m')
+print('Obrigado por utilizar programa de análise de jogadores! Volte sempre!')
